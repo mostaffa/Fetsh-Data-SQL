3f8bec63c76af96ad71d7f5f135a1c8b8072e914
Mostafa <smogeq7@yahoo.com>
2020-07-03 23:05:19 +0200

    add files

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const https = require('https');
 const fs = require('fs');
 const path = require("path")
-console.log(path.join(__dirname+"/data.json"))
-const url = "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_COVID19/FeatureServer/0/query?where=1%3D1&outFields=Bundesland,IdBundesland,AnzahlFall,Landkreis&outSR=4326&f=json"
+const mysql = require("mysql");
+
+const connectionString = mysql.createConnection({
+    host: '192.168.178.4/phpmyadmin',
+    user: 'pi',
+    password: '36862',
+    database: 'OwnCloud'
+});
+connectionString.connect();
+
+const url = "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/Coronaf%C3%A4lle_in_den_Bundesl%C3%A4ndern/FeatureServer/0/query?where=1%3D1&outFields=OBJECTID_1,OBJECTID,Fallzahl,Shape__Length,Death,Aktualisierung&returnGeometry=false&outSR=4326&f=json"
 https.get(url, res=>{
     let data = "";
     res.on('data', chunk=>{
@@ -11,6 +20,7 @@ https.get(url, res=>{
 
     res.on('end',()=>{
        let obj = JSON.parse(data)['features']
+       console.log("We HAve : "+ obj.length+" Record")
         //console.log(data)
         fs.writeFile(path.join(__dirname+"/data.json"),JSON.stringify(obj),"utf8",(error,d)=>{
             if(error)
