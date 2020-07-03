ea76d54958c43a9aa033b8eaf787d0384a169ada
Mostafa Othman <smogeq7@yahoo.com>
2020-07-04 00:31:18 +0200

    complete task

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const path = require("path")
 const mysql = require("mysql");
 
 const connectionString = mysql.createConnection({
-    host: '192.168.178.4/phpmyadmin',
+    host: 'localhost',
     user: 'pi',
     password: '36862',
     database: 'OwnCloud'
 });
-connectionString.connect();
+//connectionString.connect();
 
 const url = "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/Coronaf%C3%A4lle_in_den_Bundesl%C3%A4ndern/FeatureServer/0/query?where=1%3D1&outFields=OBJECTID_1,OBJECTID,Fallzahl,Shape__Length,Death,Aktualisierung&returnGeometry=false&outSR=4326&f=json"
 https.get(url, res=>{
@@ -20,6 +20,22 @@ https.get(url, res=>{
 
     res.on('end',()=>{
        let obj = JSON.parse(data)['features']
+       // coonection
+       connectionString.connect();
+       obj.forEach((record,index) => {
+
+           let v1 = parseInt(obj[index].attributes.OBJECTID);
+           let v2 = parseInt(obj[index].attributes.Fallzahl);
+           //INSERT INTO `Corona`( `Bundesland`, `AnfallZahl`) VALUES (4,4)
+           let insertString = 'INSERT INTO OwnCloud.Corona VALUES (null,'+v1+','+v2+')'
+           console.log(insertString);
+           let q = connectionString.query(insertString, (error, reponse)=>{
+               if(error){
+                   console.log(error)
+               }
+           })
+       });
+       connectionString.end()
        console.log("We HAve : "+ obj.length+" Record")
         //console.log(data)
         fs.writeFile(path.join(__dirname+"/data.json"),JSON.stringify(obj),"utf8",(error,d)=>{
